920549b596223994f9ca6d5f5b1b2abfee1afa9e
Robert Swain <robert.swain@gmail.com>
2020-11-03 23:24:49 +0100

    Fix formatting

--- src_debug_ui/systems.rs
+++ src_debug_ui/systems.rs
@@ -2,7 +2,8 @@ use bevy::prelude::*;
 use rapier::pipeline::PhysicsPipeline;
 
 pub fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font_handle = asset_server.load(format!("{}/../assets/FiraSans-Bold.ttf", env!("CARGO_MANIFEST_DIR")).as_str());
+    let font_handle = asset_server
+        .load(format!("{}/../assets/FiraSans-Bold.ttf", env!("CARGO_MANIFEST_DIR")).as_str());
     commands
         // 2d camera
         .spawn(UiCameraComponents::default())
