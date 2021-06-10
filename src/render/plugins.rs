6d6732e63fe7dac19c36c913e73752a4fbc6fc43
Robert Swain <robert.swain@gmail.com>
2021-06-10 12:53:06 +0200

    Add and export labels for systems
    This allows developers using bevy_rapier to explicitly schedule their systems
    relative to bevy_rapier systems, either through use of stages, or the new
    before/after system dependency specification within a stage.

--- src/render/plugins.rs
+++ src/render/plugins.rs
@@ -8,11 +8,15 @@ impl Plugin for RapierRenderPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_system_to_stage(
             CoreStage::PreUpdate,
-            systems::create_collider_renders_system.system(),
+            systems::create_collider_renders_system
+                .system()
+                .label(systems::CREATE_COLLIDER_RENDERS_SYSTEM),
         );
         app.add_system_to_stage(
             CoreStage::PreUpdate,
-            systems::update_collider_render_mesh.system(),
+            systems::update_collider_render_mesh
+                .system()
+                .label(systems::UPDATE_COLLIDER_RENDER_MESH_SYSTEM),
         );
     }
 }
