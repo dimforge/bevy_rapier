9a78983908593ab6c705099de1b47e89755f7d22
Vladyslav Batyrenko <mvlabat@gmail.com>
2022-05-01 22:48:18 +0300

    Add a missing TimestepMode export
    Co-authored-by: Sébastien Crozet <sebastien@crozet.re>

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -1,4 +1,4 @@
-pub use self::configuration::{RapierConfiguration, SimulationToRenderTime};
+pub use self::configuration::{RapierConfiguration, SimulationToRenderTime, TimestepMode};
 pub use self::context::RapierContext;
 pub use self::plugin::{NoUserData, RapierPhysicsPlugin};
 
