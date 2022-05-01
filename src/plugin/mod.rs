7ede576fae88fab41ce15e0479899608ac0c019d
mvlabat <mvlabat@gmail.com>
2022-05-01 14:27:49 +0300

    Allow missing docs for systems

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -2,6 +2,7 @@ pub use self::configuration::RapierConfiguration;
 pub use self::context::RapierContext;
 pub use self::plugin::{NoUserData, RapierPhysicsPlugin};
 
+#[allow(missing_docs)]
 pub mod systems;
 
 mod configuration;
