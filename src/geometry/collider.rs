8240b9d43ff405ce11f55328a4a16e038b3cf823
Aceeri <conmcclusk@gmail.com>
2022-06-10 17:58:12 -0700

    Make `Sensor` a marker component, handle removal of it as well

--- src/geometry/collider.rs
+++ src/geometry/collider.rs
@@ -79,15 +79,9 @@ pub enum ColliderScale {
 }
 
 /// Indicates whether or not the collider is a sensor.
-#[derive(Copy, Clone, Debug, PartialEq, Component, Reflect, FromReflect)]
+#[derive(Copy, Clone, Default, Debug, PartialEq, Component, Reflect, FromReflect)]
 #[reflect(Component, PartialEq)]
-pub struct Sensor(pub bool);
-
-impl Default for Sensor {
-    fn default() -> Self {
-        Self(true)
-    }
-}
+pub struct Sensor;
 
 /// Custom mass-properties of a collider.
 #[derive(Copy, Clone, Debug, PartialEq, Component, Reflect, FromReflect)]
