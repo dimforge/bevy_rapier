b69f34812a9680530d81bff406ce93029c470ea3
nezuo <hinckleytmicah@gmail.com>
2022-06-19 14:58:32 -0700

    Run debug_render_scene before lines are drawn

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -78,7 +78,10 @@ impl Plugin for RapierDebugRenderPlugin {
                 enabled: true,
                 pipeline: DebugRenderPipeline::new(self.style, self.mode),
             })
-            .add_system_to_stage(CoreStage::PostUpdate, debug_render_scene);
+            .add_system_to_stage(
+                CoreStage::PostUpdate,
+                debug_render_scene.before("draw_lines"),
+            );
     }
 }
 
