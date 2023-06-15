ee5460a277bd1d828158c578d2cf5ba2668bb138
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-06-15 18:50:04 -0400

    Revert "System reordering"
    This reverts commit a50235a553785dccc67a3739191564fbbf9fc90e.

--- src/plugin/plugin.rs
+++ src/plugin/plugin.rs
@@ -86,23 +86,25 @@ where
                     .after(systems::update_character_controls)
                     .in_set(PropagateTransformsSet)
                     .in_set(RapierTransformPropagateSet),
+            )
+                .into_configs(),
+            PhysicsSet::SyncBackend2 => (
                 systems::init_async_colliders.after(RapierTransformPropagateSet),
-                #[cfg(all(feature = "dim3", feature = "async-collider"))]
-                systems::init_async_scene_colliders
-                    .after(bevy::scene::scene_spawner_system)
-                    .before(systems::init_async_colliders),
                 systems::apply_scale.after(systems::init_async_colliders),
                 systems::apply_collider_user_changes.after(systems::apply_scale),
                 systems::apply_rigid_body_user_changes.after(systems::apply_collider_user_changes),
                 systems::apply_joint_user_changes.after(systems::apply_rigid_body_user_changes),
                 systems::init_rigid_bodies.after(systems::apply_joint_user_changes),
                 systems::sync_velocity.after(systems::init_rigid_bodies),
-            )
-                .into_configs(),
-            PhysicsSet::SyncBackend2 => (
-                systems::init_colliders,
+                systems::init_colliders
+                    .after(systems::sync_velocity)
+                    .after(systems::init_async_colliders),
                 systems::init_joints.after(systems::init_colliders),
                 systems::apply_initial_rigid_body_impulses.after(systems::init_colliders),
+                #[cfg(all(feature = "dim3", feature = "async-collider"))]
+                systems::init_async_scene_colliders
+                    .after(bevy::scene::scene_spawner_system)
+                    .before(systems::init_async_colliders),
             )
                 .into_configs(),
             PhysicsSet::SyncBackendFlush => (apply_system_buffers,).into_configs(),
