ca5d879356391d00b9d59d6a873a37ad43854001
Anthony Tornetta <cornchipgonecodin@gmail.com>
2024-05-29 11:01:54 -0400

    Merge writeback

--- src/plugin/systems/writeback.rs
+++ src/plugin/systems/writeback.rs
@@ -7,30 +7,37 @@ use bevy::prelude::*;
 
 /// System responsible for writing updated mass properties back into the [`ReadMassProperties`] component.
 pub fn writeback_mass_properties(
-    mut context: ResMut<RapierContext>,
+    context: Res<RapierContext>,
     config: Res<RapierConfiguration>,
 
     mut mass_props: Query<&mut ReadMassProperties>,
     mut mass_modified: EventReader<MassModifiedEvent>,
 ) {
-    let context = &mut *context;
+    if !config.physics_pipeline_active {
+        return;
+    }
 
-    if config.physics_pipeline_active {
+    for (_, world) in context.worlds.iter() {
         for entity in mass_modified.read() {
-            if let Some(handle) = context.entity2body.get(entity).copied() {
-                if let Some(rb) = context.bodies.get(handle) {
-                    if let Ok(mut mass_props) = mass_props.get_mut(**entity) {
-                        let new_mass_props =
-                            MassProperties::from_rapier(rb.mass_properties().local_mprops);
+            let Some(handle) = world.entity2body.get(entity).copied() else {
+                continue;
+            };
 
-                        // NOTE: we write the new value only if there was an
-                        //       actual change, in order to not trigger bevy’s
-                        //       change tracking when the values didn’t change.
-                        if mass_props.get() != &new_mass_props {
-                            mass_props.set(new_mass_props);
-                        }
-                    }
-                }
+            let Some(rb) = world.bodies.get(handle) else {
+                continue;
+            };
+
+            let Ok(mut mass_props) = mass_props.get_mut(**entity) else {
+                continue;
+            };
+
+            let new_mass_props = MassProperties::from_rapier(rb.mass_properties().local_mprops);
+
+            // NOTE: we write the new value only if there was an
+            //       actual change, in order to not trigger bevy’s
+            //       change tracking when the values didn’t change.
+            if mass_props.get() != &new_mass_props {
+                mass_props.set(new_mass_props);
             }
         }
     }
