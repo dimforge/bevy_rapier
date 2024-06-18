bbd608cc43c419c7c797c81408c5d7cd61e82c68
Thierry Berger <contact@thierryberger.com>
2024-06-18 12:15:38 +0200

    chore: publish script edits readme path to current directory

--- publish.sh
+++ publish.sh
@@ -10,6 +10,7 @@ cp -r LICENSE README.md "$tmp"/.
 
 ### Publish the 2D version.
 sed 's#\.\./src#src#g' bevy_rapier2d/Cargo.toml > "$tmp"/Cargo.toml
+gawk -i inplace '{ gsub("../README.md", "README.md") }; { print }' "$tmp"/Cargo.toml
 currdir=$(pwd)
 cd "$tmp" && cargo publish
 cd "$currdir" || exit
@@ -17,6 +18,7 @@ cd "$currdir" || exit
 
 ### Publish the 3D version.
 sed 's#\.\./src#src#g' bevy_rapier3d/Cargo.toml > "$tmp"/Cargo.toml
+gawk -i inplace '{ gsub("../README.md", "README.md") }; { print }' "$tmp"/Cargo.toml
 cp -r LICENSE README.md "$tmp"/.
 cd "$tmp" && cargo publish
 
