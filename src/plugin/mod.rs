2dec9374a277c69e845e3241d5b51d89604586d8
Thierry Berger <contact@thierryberger.com>
2024-07-05 17:19:34 +0200

    make ContactPairView public

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -1,6 +1,7 @@
 pub use self::configuration::{RapierConfiguration, SimulationToRenderTime, TimestepMode};
 pub use self::context::RapierContext;
 pub use self::plugin::{NoUserData, PhysicsSet, RapierPhysicsPlugin, RapierTransformPropagateSet};
+pub use narrow_phase::ContactPairView;
 
 #[allow(clippy::type_complexity)]
 #[allow(clippy::too_many_arguments)]
