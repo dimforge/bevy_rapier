bc53a1d661102a50486f96cfa42d5b833a1293d8
Thierry Berger <contact@thierryberger.com>
2025-05-05 10:24:20 +0200

    fix debugdump ; use git dep

--- bevy_rapier2d/examples/debugdump2.rs
+++ bevy_rapier2d/examples/debugdump2.rs
@@ -2,33 +2,32 @@
 //! run with:
 //! `cargo run --example debugdump2 > dump.dot && dot -Tsvg dump.dot > dump.svg`
 
-// FIXME: Uncomment once `bevy_mod_debugdump` gets updated to be compatible with bevy 0.16
-// use bevy::prelude::*;
-// use bevy_mod_debugdump::{schedule_graph, schedule_graph_dot};
-// use bevy_rapier2d::prelude::*;
+use bevy::prelude::*;
+use bevy_mod_debugdump::{schedule_graph, schedule_graph_dot};
+use bevy_rapier2d::prelude::*;
 
 fn main() {
-//     let mut app = App::new();
-//     app.add_plugins((
-//         DefaultPlugins,
-//         RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
-//         RapierDebugRenderPlugin::default(),
-//     ));
+    let mut app = App::new();
+    app.add_plugins((
+        DefaultPlugins,
+        RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
+        RapierDebugRenderPlugin::default(),
+    ));
 
-//     let debugdump_settings = schedule_graph::Settings {
-//         include_system: Some(Box::new(|system: &(dyn System<In = (), Out = ()>)| {
-//             if system.name().starts_with("bevy_pbr")
-//                 || system.name().starts_with("bevy_render")
-//                 || system.name().starts_with("bevy_gizmos")
-//                 || system.name().starts_with("bevy_winit")
-//                 || system.name().starts_with("bevy_sprite")
-//             {
-//                 return false;
-//             }
-//             true
-//         })),
-//         ..Default::default()
-//     };
-//     let dot = schedule_graph_dot(&mut app, PostUpdate, &debugdump_settings);
-//     println!("{dot}");
+    let debugdump_settings = schedule_graph::Settings {
+        include_system: Some(Box::new(|system| {
+            if system.name().starts_with("bevy_pbr")
+                || system.name().starts_with("bevy_render")
+                || system.name().starts_with("bevy_gizmos")
+                || system.name().starts_with("bevy_winit")
+                || system.name().starts_with("bevy_sprite")
+            {
+                return false;
+            }
+            true
+        })),
+        ..Default::default()
+    };
+    let dot = schedule_graph_dot(&mut app, PostUpdate, &debugdump_settings);
+    println!("{dot}");
 }
